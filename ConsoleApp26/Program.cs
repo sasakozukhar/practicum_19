56f79b3fd73c4a12a335329a46a1eb8f7684aa61
sasakozukhar <sasakozuhar93@gmail.com>
2025-02-02 10:20:16 +0200

    Add files via upload

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double distance = 10; 
+            double increaseRate = 0.1; 
+            int day = 1;
+
+            while (distance <= 20)
+            {
+                distance *= (1 + increaseRate);
+                day++;
+            }
+
+            Console.WriteLine($"Лыжник пробежит больше 20 км на {day}-й день.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             double dailyDistance = 10;
+            double totalDistance = 0;
+            double increaseRate = 0.1;
+            int day = 1;
+
+            while (totalDistance <= 100)
+            {
+                totalDistance += dailyDistance;
+                dailyDistance *= (1 + increaseRate);
+                day++;
+            }
+
+            Console.WriteLine($"Суммарный пробег превысит 100 км на {day}-й день.");
+            Console.ReadKey();
+        }
+    }
+}
