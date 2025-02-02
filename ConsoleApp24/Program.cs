01ce9b4f0c871a9d22aa5821696dd998befd9a3a
sasakozukhar <sasakozuhar93@gmail.com>
2025-02-02 10:09:51 +0200

    Add files via upload

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double initialSavings = 100;
+            double monthlyIncrease = 50;
+            int numberOfMonths = 10;
+            double totalSavings = 0;
+
+            for (int i = 0; i < numberOfMonths; i++)
+            {
+                totalSavings += initialSavings + i * monthlyIncrease;
+            }
+
+            Console.WriteLine($"Общая сумма через {numberOfMonths} месяцев: {totalSavings}");
+            Console.ReadKey();
+        }
+    }
+}
