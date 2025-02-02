ea50c1088699b499f82863c03fa0bf7dc8df8342
sasakozukhar <sasakozuhar93@gmail.com>
2025-02-02 09:57:03 +0200

    Add files via upload

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int a1 = 1; 
+            int d = 4;  
+
+          
+            for (int i = 1; i <= 5; i++)
+            {
+                int an = a1 + (i - 1) * d;
+                Console.Write(an + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
