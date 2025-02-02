3abd494f86feb128e2df4f8c3ee9333faca2c59d
sasakozukhar <sasakozuhar93@gmail.com>
2025-02-02 10:14:10 +0200

    Add files via upload

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a1 = 200; 
+            double d = 0.2;  
+            int n = 150;     
+
+            
+            double sum = n / 2.0 * (2 * a1 + (n - 1) * d);
+
+            Console.WriteLine($"Сумма первых {n} членов арифметической прогрессии: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
